f20ebc3056d0a05fe27b3fa0f9ca998a5dc4016a
zijun <jim55525@gmail.com>
2022-12-08 00:04:52 +0800

    init

---
+++ app/src/main/java/com/example/ordersystem/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.ordersystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Spinner sp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sp =  findViewById(R.id.spinner);
+
+        // 設定 sp 元件 ItemSelected 事件的 listener
+        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView parent, View view, int position, long id) {
+                String result = parent.getItemAtPosition(position).toString();
+                Toast.makeText(MainActivity.this, result, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView parent) {
+
+            }
+        });
+    }
+}
